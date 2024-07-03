57cf917ed21d225d0c25ab69c8b8ef8e53079ec5
dampa1987 <dampa1987@gmail.com>
2024-07-03 16:20:43 +0900

    初回コミット

---
+++ Assets/CoinObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コインの挙動
+/// </summary>
+public class CoinObject : MonoBehaviour
+{
+	//Update処理は毎回呼ばれる
+	void Update()
+	{
+		//一定以下の高さになった場合
+		if (transform.localPosition.y <= -7)
+		{
+			//オブジェクト削除
+			Destroy(gameObject);
+		}
+	}
+}
