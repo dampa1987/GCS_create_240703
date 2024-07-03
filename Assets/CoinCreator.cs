3aeeb232bdcb2d1be34acc45576054fedafdeab2
dampa1987 <dampa1987@gmail.com>
2024-07-03 16:33:07 +0900

    初回コミット

---
+++ Assets/CoinCreator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コインの作成を行う
+/// </summary>
+public class CoinCreator : MonoBehaviour
+{
+	//コピーする元となるコイン
+	[SerializeField] private GameObject _originalCoin;
+
+	//Update処理は毎回呼ばれる
+	void Update()
+	{
+		//マウスクリックをした
+		if (Input.GetMouseButtonDown(0))
+		{
+			//クリック地点からレイを作成する
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			//レイの先にオブジェクトがある
+			if (Physics.Raycast(ray, out hit, 500))
+			{
+				//コインの生成位置の算出
+				var distance = Vector3.Distance(Camera.main.transform.position, hit.point);
+				//クリック地点
+				var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+				//クリック地点をワールド座標に変換
+				var position = Camera.main.ScreenToWorldPoint(mousePosition);
+				//少し高い位置に生成
+				position.y += 5;
+				//コインの生成
+				var objectBase = (GameObject)Instantiate(_originalCoin);
+				//算出した位置を設定
+				objectBase.gameObject.transform.localPosition = position;
+				//有効化する
+				objectBase.gameObject.SetActive(true);
+			}
+		}
+	}
+}
---
+++ Assets/MoveStand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 台座の移動
+/// </summary>
+public class MoveStand : MonoBehaviour
+{
+	//奥に移動
+	private bool _isBack = false;
+
+	//Update処理は毎回呼ばれる
+	void Update()
+	{
+		//台の位置
+		var position = transform.localPosition;
+		if (_isBack)
+		{
+			//奥に移動
+			position.z += Time.deltaTime * 2;
+		}
+		else
+		{
+			//手前に移動
+			position.z -= Time.deltaTime * 2;
+		}
+		if (position.z >= 7)
+		{
+			//ある程度奥に行ったら奥に行くフラグをオフ
+			_isBack = false;
+		}
+		else if (position.z <= 4)
+		{
+			//ある程度手前なら奥に行くフラグをオン
+			_isBack = true;
+		}
+		//移動位置を適応
+		transform.localPosition = position;
+	}
+}
